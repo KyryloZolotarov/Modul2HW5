2dc413ea063f831021b083bd3eeee3589f20c077
Kyrylo Zolotarov <zlatashay@gmail.com>
2023-04-13 00:00:57 +0300

    done!

---
+++ Modul2HW5/Modul2HW5/Logger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul2HW5
+{
+    internal class Logger
+    {
+        private static Logger? instance;
+        private readonly FileService _storage;
+        private Logger()
+        {
+            _storage = new FileService();
+        }
+
+        public static Logger GetInstance()
+        {
+            instance ??= new Logger();
+
+            return instance;
+        }
+
+        public string DispLog()
+        {
+            return _storage.DisplayLogs();
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(LogLevel.Info, message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Log(LogLevel.Warning, message);
+        }
+
+        public void LogError(string message)
+        {
+            Log(LogLevel.Error, message);
+        }
+
+        public void SaveToFile()
+        {
+            _storage.SaveToFile();
+        }
+
+        private void Log(LogLevel level, string message)
+        {
+            _storage.StoreLog($"{DateTime.Now.ToShortTimeString()}: {level} : {message}");
+        }
+    }
+}
---
+++ Modul2HW5/Modul2HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Modul2HW5
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            new Starter().Run();
+            Console.WriteLine(Logger.GetInstance().DispLog());
+            Logger.GetInstance().SaveToFile();
+        }
+    }
+}
